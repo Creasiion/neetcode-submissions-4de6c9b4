cbf4f810f782f7ac9e9b4b4d99b9384f056e5867
Creasion <129997790+Creasiion@users.noreply.github.com>
2026-08-25 00:03:14 -0700

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Get a temp variable + set it to a node's next
+        # move that node's next to the prev value
+        # Since we don't have access to their old next,
+        # we use temp for this reason!
+        # Would also need a prev value so then it can go
+        # head.next = prev
+        # prev = head
+        # head = temp (something along those lines)
+        curr = head
+        prev = None
+        while curr:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
+        return prev
+    
+        
